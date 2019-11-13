55685bac9b0b6fa06727b2e922b7459cb6393720
jkmchinoxxx <31919649+jkmchinoxxx@users.noreply.github.com>
2019-11-13 13:44:16 +0300

    Update settings.py

--- fourthapp/settings.py
+++ fourthapp/settings.py
@@ -150,6 +150,8 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_DIRS = [
